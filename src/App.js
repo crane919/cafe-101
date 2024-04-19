b9889c1145da9546606297a3290311366edabf06
Maya Cranor <mcranor@olin.edu>
2024-04-18 22:20:38 -0400

    Started making buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react'
+import Selector from './Selector'
+
+    
+// App component
 function App() {
+
+  // Define drink orders
+  const drink_order = {
+    "Base": "Hot Choccy",
+    "Toppings": ["Whipped Cream"],
+    "Notes": "NA"
+  };
+  const [order, updateOrder] = useState(drink_order)
+  
+  // Define drink names
+  const drink1 = "Chocolate";
+  const drink2 = "Tea Latte";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Cafe 101</p>
       </header>
+      <div>
+        <p>
+          Welcome to the Cafe 101 website: Your current drink order is {drink_order["Base"]} with {drink_order["Toppings"]}. Special notes include: {drink_order["Notes"]}.
+        </p>
+        <Selector onClick={updateBase} text={drink1} />
+        <Selector onClick={updateBase} text={drink2} />
+      </div>
     </div>
   );
 }
---
+++ src/Selector.js
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+export default function Selector ({text}){
+    return (
+        <button>
+            {text}
+        </button>
+      
+    )
+}
+    
