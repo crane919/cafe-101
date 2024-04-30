a8b56dde47f8ba175f22a90cfcd8fb64fd3a0d4e
AJ <ajecoder@gmail.com>
2024-04-30 13:07:56 -0400

    right panel refreshing on order submission

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+
 export default function Fetch ({orders, setOrders}) {
     if (orders == null) {
         console.log('Getting Orders...')
         fetch('http://127.0.0.1:5000/orders').then(response=>response.json().then (j => setOrders(j.orders)))
         return <p>No Orders.</p>
     } else {
-        console.log(orders)
+        fetch('http://127.0.0.1:5000/orders').then(response=>response.json().then (j => setOrders(j.orders)))
+        // console.log(orders)
         const drinks = orders.map(({name, order})=> (
             <div className='order-item'>
                 <p className='order-name'>Name: {name}</p>
@@ -19,6 +21,7 @@ export default function Fetch ({orders, setOrders}) {
                         ))
                         }
                     </ul>
+                
                 </div>
             </div>
         ))
